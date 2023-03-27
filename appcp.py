2d6fa6aa348794abaefe5a94430f33918d1b6d11
Maither <58180940+Maither@users.noreply.github.com>
2023-03-27 13:28:28 +0200

    Update appcp.py

--- appcp.py
+++ appcp.py
@@ -1,7 +1,19 @@
+import datetime
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+# Define the Temperature model
+class Temperature(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_time = db.Column(db.DateTime, nullable=False)
+    temperature = db.Column(db.Float, nullable=False)
+        def __repr__(self):
+        return '<Temperature {}>'.format(self.temperature)
 
 @app.route("/")
 def homepage():
@@ -10,14 +22,22 @@ def homepage():
 @app.route("/get_json", methods=['POST'])
 def get_post_temp():
     data = request.get_json()
-    d = data['date']
+    y = data['year']
+    mo = data['month']
+    d = data['day']
     h = data['hour']
-    m = data['min']
-    s = data['sec']
-    t = data['temp']
-    data = [d, h, m, s, t]
+    mi = data['minute']
+    s = data['second']
+    t = float(data['temp'])
+    dt = datetime.datetime(y, mo, d, h, mi, s)
+    new_temp = Temperature(date_time=dt, temperature=t)
+    db.session.add(new_temp)
+    db.session.commit()
     
+    #add datetime and temp to db
     return jsonify('200')
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
