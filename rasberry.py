5776b504cf319885f02be8cf9f54fb7d2c07b7a1
Maither <58180940+Maither@users.noreply.github.com>
2023-03-28 22:01:26 +0200

    Update rasberry.py

--- rasberry.py
+++ rasberry.py
@@ -13,17 +13,17 @@ Session = sqlalchemy.orm.sessionmaker(bind=engine)
 # Define the Temperature model
 class Temperature(Base):
     __tablename__ = 'temperature'
-    
+
     id = Column(Integer, primary_key=True)
     date_time = Column(DateTime, nullable=False)
     temperature = Column(Float, nullable=False)
-    
-    
+
+
     def commit(self):
         session = Session()
         session.add(self)
         session.commit()
-        
+
     def post(self, url='http://mether.fr/get_json'):
         session = Session()
         session.add(self)
@@ -36,27 +36,27 @@ class Temperature(Base):
             else:
                 print(response.status_code, 'Error posting temperature')
                 time.sleep(1)
-                
+
 
 
 def main():
     Base.metadata.create_all(engine)
-    
+
     while True:
         temperature = get_temperature()
         if temperature:
             now = datetime.now()
             print(now, temperature)
-            
+
             tmp = Temperature(date_time=now, temperature=temperature)
             tmp.commit()
             tmp.post()
-            
+
             time.sleep(600)
         else:
              time.sleep(1)
-	
-	
+
+
 def get_temperature(route="/sys/bus/w1/devices/28*/w1_slave"):
     """
     return a float if expected beavior else false
@@ -67,9 +67,6 @@ def get_temperature(route="/sys/bus/w1/devices/28*/w1_slave"):
     #if there is a file else return false
     if len(route_capteurs) > 0:
         with open(route_capteurs[0]) as f:
-	    file = f.read()
-	    print(file)
-	    return False
             #if this is a unexpected string return false
             try:
                 for i, char in enumerate(f.read()):
@@ -77,9 +74,9 @@ def get_temperature(route="/sys/bus/w1/devices/28*/w1_slave"):
                         return float(f.read()[i:])/1000
             except ValueError:
                 return False
-    
+
     return False
 
 
 if __name__ == "__main__":
-    main()
+    main() 1 contributor 
